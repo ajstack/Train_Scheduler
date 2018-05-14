7d5c045ca1a6260f957a8f20a4f6ac9ee8a91aed
Alice <a.j.stack@sbcglobal.net>
2018-05-13 19:13:40 -0500

    Finished bootstrap layout

---
+++ app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyBs646z5wUS8Pq_vg5098TB6ch9jEV2wH0",
+    authDomain: "train-scheduler-b9898.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b9898.firebaseio.com",
+    projectId: "train-scheduler-b9898",
+    storageBucket: "train-scheduler-b9898.appspot.com",
+    messagingSenderId: "231611321866"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
